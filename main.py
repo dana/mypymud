a4fcf921fda68e4c0efb59e7f2fee50ac2b48f18
Dana M. Diederich <diederich@gmai.com>
2024-12-29 10:33:00 -0800

    Don't need the comments

--- main.py
+++ main.py
@@ -6,32 +6,25 @@ def start_server():
     host = '0.0.0.0'
     port = 5000
 
-    # Create a socket object
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    # Bind the socket to the address and port
     server_socket.bind((host, port))
 
-    # Listen for incoming connections
     server_socket.listen(1)
 
     print(f"Server listening on {host}:{port}")
 
     while True:
-        # Accept a connection
         client_socket, client_address = server_socket.accept()
         print(f"Connection from {client_address}")
 
-        # Receive data from the client
         data = client_socket.recv(1024)
         print(f"Received: {data.decode()}")
 
-        # Send a response to the client
         message = "Hello from the server!"
         client_socket.send(message.encode())
 
-        # Close the client socket
         client_socket.close()
 
 if __name__ == "__main__":
