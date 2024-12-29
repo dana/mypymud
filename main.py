a9cd00e2e5aa9851d2230796574d71e796f0122f
Dana M. Diederich <diederich@gmai.com>
2024-12-29 11:33:42 -0800

    moved the networking stuff into a the MUDNetwork class

--- main.py
+++ main.py
@@ -6,59 +6,7 @@ import sys
 sys.path.append('mypymud')
 import mudnetwork
 
-all_sockets = []
+mn = mudnetwork.MUDNetwork()
 
-def accept_wrapper(sock):
-    conn, addr = sock.accept() 
-    print(f"Accepted connection from {addr}")
-    conn.setblocking(False)
-    data = {"addr": addr, "in_buffer": b"", "out_buffer": b""}
-    events = selectors.EVENT_READ | selectors.EVENT_WRITE
-    sel.register(conn, events, data=data)
-    all_sockets.append({"conn":conn, "events":events, "data":data})
-
-def service_connection(key, mask):
-    sock = key.fileobj
-    data = key.data
-
-    if mask & selectors.EVENT_READ:
-        recv_data = sock.recv(1024) 
-        if recv_data:
-            data["in_buffer"] += recv_data
-            for s in all_sockets:
-                s["data"]["out_buffer"] += recv_data
-        else:
-            print(f"Closing connection to {data['addr']}")
-            sel.unregister(sock)
-            sock.close()
-
-    if mask & selectors.EVENT_WRITE:
-        if data["out_buffer"]:
-            print(f"Echoing {data['out_buffer']!r} to {data['addr']}")
-            sent = sock.send(data["out_buffer"])
-            data["out_buffer"] = data["out_buffer"][sent:]
-
-sel = selectors.DefaultSelector()
-
-host = "0.0.0.0" 
-port = 5000
-
-lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-lsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-lsock.bind((host, port))
-lsock.listen()
-print(f"Listening on {(host, port)}")
-lsock.setblocking(False)
-sel.register(lsock, selectors.EVENT_READ, data=None)
-
-try:
-    while True:
-        events = sel.select(timeout=None)
-        for key, mask in events:
-            if key.data is None:
-                accept_wrapper(key.fileobj)
-            else:
-                service_connection(key, mask)
-finally:
-    sel.close()
-
+mn.bind()
+mn.loop()
--- mypymud/mudnetwork.py
+++ mypymud/mudnetwork.py
@@ -1,11 +1,69 @@
+import selectors
+import socket
+
 class MUDNetwork:
     def __init__(self):
+        self.all_sockets = []
+        print("init")
         pass
+
+    def _accept_wrapper(self, sock):
+        conn, addr = sock.accept()
+        print(f"Accepted connection from {addr}")
+        conn.setblocking(False)
+        data = {"addr": addr, "in_buffer": b"", "out_buffer": b""}
+        events = selectors.EVENT_READ | selectors.EVENT_WRITE
+        self.sel.register(conn, events, data=data)
+        self.all_sockets.append({"conn":conn, "events":events, "data":data})
+
+    def _service_connection(self, key, mask):
+        sock = key.fileobj
+        data = key.data
+
+        if mask & selectors.EVENT_READ:
+            recv_data = sock.recv(1024)
+            if recv_data:
+                data["in_buffer"] += recv_data
+                for s in self.all_sockets:
+                    s["data"]["out_buffer"] += recv_data
+            else:
+                print(f"Closing connection to {data['addr']}")
+                self.sel.unregister(sock)
+                sock.close()
+
+        if mask & selectors.EVENT_WRITE:
+            if data["out_buffer"]:
+                print(f"Echoing {data['out_buffer']!r} to {data['addr']}")
+                sent = sock.send(data["out_buffer"])
+                data["out_buffer"] = data["out_buffer"][sent:]
+
+
+
 
     def bind(self):
-        pass
+        self.sel = selectors.DefaultSelector()
+
+        host = "0.0.0.0"
+        port = 5000
+
+        self.lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.lsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.lsock.bind((host, port))
+        self.lsock.listen()
+        print(f"Listening on {(host, port)}")
+        self.lsock.setblocking(False)
+        self.sel.register(self.lsock, selectors.EVENT_READ, data=None)
+
 
     def loop(self):
-        pass
-
+        try:
+            while True:
+                events = self.sel.select(timeout=None)
+                for key, mask in events:
+                    if key.data is None:
+                        self._accept_wrapper(key.fileobj)
+                    else:
+                        self._service_connection(key, mask)
+        finally:
+            self.sel.close()
 
