4f354b261881eac8e164d2474b68c7d382bdfaab
Dana M. Diederich <diederich@gmai.com>
2024-12-29 10:43:02 -0800

    More appropriate simple multiplexing TCP server from google

--- main.py
+++ main.py
@@ -1,36 +1,57 @@
 #!/usr/local/bin/python3
 
+import selectors
 import socket
 
-client_sockets = []
+def accept_wrapper(sock):
+    conn, addr = sock.accept() 
+    print(f"Accepted connection from {addr}")
+    conn.setblocking(False)
+    data = {"addr": addr, "in_buffer": b"", "out_buffer": b""}
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
 
-def start_server():
-    host = '0.0.0.0'
-    port = 5000
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
 
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024) 
+        if recv_data:
+            data["in_buffer"] += recv_data
+            data["out_buffer"] += recv_data
+        else:
+            print(f"Closing connection to {data['addr']}")
+            sel.unregister(sock)
+            sock.close()
 
-    server_socket.bind((host, port))
+    if mask & selectors.EVENT_WRITE:
+        if data["out_buffer"]:
+            print(f"Echoing {data['out_buffer']!r} to {data['addr']}")
+            sent = sock.send(data["out_buffer"])
+            data["out_buffer"] = data["out_buffer"][sent:]
 
-    server_socket.listen(1)
+sel = selectors.DefaultSelector()
 
-    print(f"Server listening on {host}:{port}")
+host = "0.0.0.0" 
+port = 5000
 
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+lsock.bind((host, port))
+lsock.listen()
+print(f"Listening on {(host, port)}")
+lsock.setblocking(False)
+sel.register(lsock, selectors.EVENT_READ, data=None)
+
+try:
     while True:
-        client_socket, client_address = server_socket.accept()
-        print(f"Connection from {client_address}")
-
-        client_socket.send("Connected to server\n".encode())
-
-        data = client_socket.recv(1024)
-        print(f"Received: {data.decode()}")
-
-        message = "Hello from the server!"
-        client_socket.send(message.encode())
-
-        client_socket.close()
-
-if __name__ == "__main__":
-    start_server()
+        events = sel.select(timeout=None)
+        for key, mask in events:
+            if key.data is None:
+                accept_wrapper(key.fileobj)
+            else:
+                service_connection(key, mask)
+finally:
+    sel.close()
 
